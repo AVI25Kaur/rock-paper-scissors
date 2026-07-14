5593bf3251303ff9a85ddb942bf1121e18ab152b
Avineet Kaur <kauravineet25@gmail.com>
2026-07-14 16:33:38 +0000

    remove playgame loop

--- script.js
+++ script.js
@@ -5,11 +5,7 @@ function getComputerChoice() {
 }
 console.log(getComputerChoice());
 
-function getUserChoice() {
-    const userInput = prompt("Enter rock, paper, or scissors:").toLowerCase();
-    return userInput;
-}
-console.log(getUserChoice());
+
 
 let humanscore = 0;
 let computerscore = 0;
@@ -31,19 +27,3 @@ function playRound(userChoice, computerChoice) {
 
 }
 
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        const userSelection = getUserChoice();
-        const computerSelection = getComputerChoice();
-        playRound(userSelection, computerSelection);
-    }
-    if (humanscore > computerscore) {
-        console.log(`You won the game! Final score: You ${humanscore} - Computer ${computerscore}`);
-    }
-    else if (humanscore < computerscore) {
-        console.log(`You lost the game! Final score: You ${humanscore} - Computer ${computerscore}`);
-    }
-    else {
-        console.log(`The game is a tie! Final score: You ${humanscore} - Computer ${computerscore}`);
-    }
-}
