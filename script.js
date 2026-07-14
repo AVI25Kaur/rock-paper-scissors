d8319b7e5e99cabdd4f77e43adae9ce4f3110bde
Avineet Kaur <kauravineet25@gmail.com>
2026-07-14 16:02:48 +0000

    add playround function

--- script.js
+++ script.js
@@ -3,5 +3,34 @@ function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
-
 console.log(getComputerChoice());
+
+function getUserChoice() {
+    const userInput = prompt("Enter rock, paper, or scissors:").toLowerCase();
+    return userInput;
+}
+console.log(getUserChoice());
+
+let humanscore = 0;
+let computerscore = 0;
+
+function playRound(userChoice, computerChoice) {
+    userChoice = userChoice.toLowerCase();
+    if (userChoice === computerChoice) {
+        console.log("It's a tie!");
+    }
+    else if ((userChoice === 'rock' && computerChoice === 'scissors') ||
+             (userChoice === 'paper' && computerChoice === 'rock') ||
+             (userChoice === 'scissors' && computerChoice === 'paper')) {
+        console.log(`You win! ${userChoice} beats ${computerChoice}`);
+        humanscore++;
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${userChoice}`);
+        computerscore++;
+    }
+
+}
+const userSelection = getUserChoice();
+const computerSelection = getComputerChoice();
+
+playRound(userSelection, computerSelection);
